52f229decbd1fa14f244506ef27c312f71dc853f
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-07-12 13:40:15 +0300

    product

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <h2>Im Header</h2>
+        <h1 className="text-3xl  underline bg-red-500">
+          Hello world!
+        </h1>
+      </header>
+    </div>
+  );
+}
+export default Header;
