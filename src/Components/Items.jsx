bb70e71abb85e92e037d64412d00fe0f32302f6d
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-07-14 23:17:13 +0300

    props

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -1,6 +1,10 @@
 export default function Items(props) {
   return (
-    <div>
+    <div style={{ background: "red", 
+    width: "150px",
+    fontSize:"20px",
+    padding:"25px",
+     }}>
       <h3>{props.name}</h3>
     </div>
   );
